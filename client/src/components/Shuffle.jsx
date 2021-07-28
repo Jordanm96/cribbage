d766d9d1a3cb91d19adcadb739ccf1f72a3e5e7e
paulmoschetti <paulmoschetti93@gmail.com>
2021-07-28 14:04:26 -0600

    deck shuffle logic

--- client/src/components/Shuffle.jsx
+++ client/src/components/Shuffle.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 
-export default function Shuffle() {
+
+export default function Shuffle(props) {
+      
+  function shuffle() {
+    let shuffledDeck = deck;
+    let currentIndex = deck.length;
+    let temporaryValue;
+    let randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      temporaryValue = shuffledDeck[currentIndex]
+  
+      shuffledDeck[currentIndex] = shuffledDeck[randomIndex]
+      shuffledDeck[randomIndex] = temporaryValue
+    }
+  
+    return shuffledDeck
+  }
+
   return (
     <div>
       
